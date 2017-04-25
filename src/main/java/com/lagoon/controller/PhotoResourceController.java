5d540451b7a94c29283d8621bb8f21ee53e38f81
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-04-25 21:51:39 +1000

    adding photo resource to update photo

--- src/main/java/com/lagoon/controller/PhotoResourceController.java
+++ src/main/java/com/lagoon/controller/PhotoResourceController.java
@@ -3,6 +3,8 @@ package com.lagoon.controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,78 +27,89 @@ import com.lagoon.service.PhotoService;
 @RequestMapping("/api")
 public class PhotoResourceController {
 
-    private String imageName;
+	private String imageName;
 
-    @Autowired
-    private PhotoService photoService;
+	@Autowired
+	private PhotoService photoService;
 
-    @RequestMapping(value = "/photo", method = RequestMethod.PUT)
-    public LagoonResult editPhoto(@RequestBody Photo photo) {
-        LagoonResult result = null;
-        try {
-            Photo updatedPhoto = this.photoService.save(photo);
-            result = LagoonResult.ok(updatedPhoto);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            result = new LagoonResult(500, ExceptionUtils.getStackTrace(e), null);
-        }
-        return result;
-    }
+	@RequestMapping(value = "/photo", method = RequestMethod.PUT)
+	public LagoonResult editPhoto(@RequestBody Photo photo) {
+		LagoonResult result = null;
+		try {
+			Photo updatedPhoto = this.photoService.save(photo);
+			result = LagoonResult.ok(updatedPhoto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = new LagoonResult(500, ExceptionUtils.getStackTrace(e), null);
+		}
+		return result;
+	}
 
-    @RequestMapping(value = "/photo/{id}", method = RequestMethod.DELETE)
-    public LagoonResult deletePhoto(@PathVariable("id") long photoId) {
-        return null;
-    }
+	@RequestMapping(value = "/photo/{id}", method = RequestMethod.DELETE)
+	public LagoonResult deletePhoto(@PathVariable("id") long photoId) {
+		return null;
+	}
 
-    @RequestMapping(value = "/photo", method = RequestMethod.POST)
-    public LagoonResult uploadPhoto(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping(value = "/photo", method = RequestMethod.POST)
+	public LagoonResult uploadPhoto(HttpServletRequest request, HttpServletResponse response) {
 
-        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-        Iterator<String> it = multipartRequest.getFileNames();
-        MultipartFile multipartFile = multipartRequest.getFile(it.next());
-        String fileName = multipartFile.getOriginalFilename();
-        setImageName(fileName);
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		Iterator<String> it = multipartRequest.getFileNames();
+		MultipartFile multipartFile = multipartRequest.getFile(it.next());
 
-        try {
-            File theFile = new File("target/classes/static/images");
-            theFile.mkdirs();
-            String path = theFile.getAbsolutePath() + "\\" + fileName;
-            multipartFile.transferTo(new File(path));
-            System.out.println(path);
-        } catch (IllegalStateException | IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return LagoonResult.build(500, ExceptionUtils.getStackTrace(e), null);
-        }
-        return LagoonResult.ok("Upload Success!");
-    }
+		Pattern compile = Pattern.compile(".*\\.(png|jpg|gif|bmp)$");
+		
+		Matcher filenameMatcher = compile.matcher(multipartFile.getOriginalFilename());
+		
+		String fileName = filenameMatcher.find()?filenameMatcher.group(0):"";
+		
+		int end = fileName.indexOf(".");
+		
+		fileName = fileName.substring(0, end);
+		
+		setImageName(fileName);
 
-    @RequestMapping(value = "/photo/add", method = RequestMethod.POST)
-    public LagoonResult addPhoto(@RequestBody Photo photo) {
-        LagoonResult result = null;
-        try {
-            String photoName = this.getImageName();
-            if (!photoName.trim().isEmpty()) {
-                photo.setImageName(photoName);
-                return result = LagoonResult.ok(photo);
-            }
+		try {
+			File theFile = new File("target/classes/static/images");
+			theFile.mkdirs();
+			String path = theFile.getAbsolutePath() + "/" + fileName;
+			multipartFile.transferTo(new File(path));
+			System.out.println(path);
+		} catch (IllegalStateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return LagoonResult.build(500, ExceptionUtils.getStackTrace(e), null);
+		}
+		return LagoonResult.ok("Upload Success!");
+	}
 
-            result = new LagoonResult(400, "photo is not yet uploaded", null);
-            
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            result = new LagoonResult(500, ExceptionUtils.getStackTrace(e), null);
-        }
-        return result;
-    }
+	@RequestMapping(value = "/photo/add", method = RequestMethod.POST)
+	public LagoonResult addPhoto(@RequestBody Photo photo) {
+		LagoonResult result = null;
+		try {
+			String photoName = this.getImageName();
+			if (!photoName.trim().isEmpty()) {
+				photo.setImageName(photoName);
+				Photo savedPhoto = photoService.save(photo);
+				return result = LagoonResult.ok(savedPhoto);
+			}
 
-    public String getImageName() {
-        return imageName != null && !imageName.trim().isEmpty() ? imageName : "";
-    }
+			result = new LagoonResult(400, "photo is not yet uploaded", null);
 
-    private void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = new LagoonResult(500, ExceptionUtils.getStackTrace(e), null);
+		}
+		return result;
+	}
+
+	public String getImageName() {
+		return imageName != null && !imageName.trim().isEmpty() ? imageName : "";
+	}
+
+	private void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
 }
